856bf1f7c360ed6691035d8cd583e047c2e369cf
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-04-27 21:59:20 -0700

    [create] main/forms.py, included TutorialForm and ResourceForm for updating and creating new tutorials and resources.

---
+++ tutorial_app/main/forms.py
@@ -0,0 +1,51 @@
+"""Main forms."""
+from flask_wtf import FlaskForm
+from wtforms import (
+    StringField,
+    SelectField,
+    SubmitField,
+    FloatField,
+    TextAreaField,
+)
+from wtforms.validators import DataRequired, Length, URL
+from tutorial_app.models import (
+    Tutorial,
+    Resource,
+    TutorialCategory,
+    Difficulty,
+)
+
+
+class TutorialForm(FlaskForm):
+    """Form for adding/updating a Tutorial."""
+
+    category = SelectField(
+        "Category",
+        choices=TutorialCategory.choices(),
+        validators=[DataRequired()],
+    )
+    title = StringField("Title", validators=[DataRequired(), Length(max=80)])
+    difficulty = SelectField(
+        "Difficulty",
+        choices=Difficulty.choices(),
+        validators=[DataRequired()],
+    )
+    body = TextAreaField(
+        "Content", validators=[DataRequired(), Length(min=5)]
+    )
+    submit = SubmitField("Submit")
+
+
+class ResourceForm(FlaskForm):
+    """Form for adding/updating a Resource."""
+
+    category = SelectField(
+        "Category",
+        choices=TutorialCategory.choices(),
+        validators=[DataRequired()],
+    )
+    title = StringField("Title", validators=[DataRequired(), Length(max=80)])
+    link = StringField(
+        "Link", validators=[DataRequired(), URL(), Length(max=200)]
+    )
+    submit = SubmitField("Submit")
