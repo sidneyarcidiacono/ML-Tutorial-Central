199ae99c6fae04a235c2862367d367da9d313b86
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-04-27 21:41:45 -0700

    [update] Include joining table for tutorials and users, updated relationships between models.

--- tutorial_app/models.py
+++ tutorial_app/models.py
@@ -29,6 +29,9 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False)
     password = db.Column(db.String(160), nullable=False)
+    saved_tutorials = db.relationship(
+        "Tutorial", secondary="saved_tutorials", back_populates="saved_by"
+    )
 
 
 class Tutorial(db.Model):
@@ -43,6 +46,11 @@ class Tutorial(db.Model):
         db.Enum(Difficulty), default=Difficulty.INTERMEDIATE
     )
     body = db.Column(db.String(10000), nullable=False)
+    saved_by = db.relationship(
+        "User",
+        secondary="saved_tutorials",
+        back_populates="saved_tutorials",
+    )
 
 
 class Resource(db.Model):
@@ -55,3 +63,10 @@ class Resource(db.Model):
     title = db.Column(db.String(40), nullable=False)
     description = db.Column(db.String(120), nullable=True)
     link = db.Column(db.String(120), nullable=False)
+
+
+saved_tutorial_table = db.Table(
+    "saved_tutorials",
+    db.Column("tutorial_id", db.Integer, db.ForeignKey("tutorial.id")),
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id")),
+)
