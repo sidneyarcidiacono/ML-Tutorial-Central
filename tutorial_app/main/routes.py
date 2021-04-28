7fd231318ccf1433b80d727c195e0bf896c8c3e4
Sidney Arcidiacono <sidneyarci@gmail.com>
2021-04-27 22:11:06 -0700

    [update] Fleshed out new_resource and new_tutorial routes to add new resources/tutorials (respectively) to database, and show success message to user.

--- tutorial_app/main/routes.py
+++ tutorial_app/main/routes.py
@@ -4,9 +4,8 @@ from flask_login import login_required, current_user
 from tutorial_app.main.forms import TutorialForm, ResourceForm
 from tutorial_app.models import Tutorial, Resource, User
 
-# from grocery_app.models import GroceryStore, GroceryItem
-# from grocery_app.main.forms import GroceryStoreForm, GroceryItemForm
-# from grocery_app import app, db
+# TODO: enable user to keep a list of their saved tutorials
+# TODO: enable user to track their own progress?
 
 main = Blueprint("main", __name__)
 
@@ -22,6 +21,17 @@ def homepage():
 def new_resource():
     """Add new tutorial or resource to the site."""
     form = ResourceForm()
+
+    if form.validate_on_submit():
+        resource = Resource(
+            category=form.category.data,
+            title=form.title.data,
+            link=form.link.data,
+        )
+        db.session.add(resource)
+        db.session.commit()
+        flash("Thank you for sharing your new resource!")
+        redirect(url_for("main.resources"))
     return render_template("new_resource.html", form=form)
 
 
@@ -30,6 +40,18 @@ def new_resource():
 def new_tutorial():
     """Add new tutorial or resource to the site."""
     form = TutorialForm()
+
+    if form.validate_on_submit():
+        tutorial = Tutorial(
+            category=form.category.data,
+            title=form.title.data,
+            difficulty=form.difficulty.data,
+            body=form.body.data,
+        )
+        db.session.add(tutorial)
+        db.session.commit()
+        flash("Thank you for adding this tutorial!")
+        redirect(url_for("main.tutorials", tutorial_id=tutorial.id))
     return render_template("new_tutorial.html", form=form)
 
 
@@ -38,7 +60,8 @@ def resources():
     """See list of resources and descriptions."""
     # Resources won't have a details page
     # All resources have a short description and an external link
-    return render_template("resources.html")
+    resources = Resource.query.all()
+    return render_template("resources.html", resources=resources)
 
 
 @main.route("/tutorials")
@@ -47,4 +70,12 @@ def all_tutorials():
     # TODO: Maybe integrate this with homepage?
     # TODO: each tutorial will have a detail page where you can
     # see and work through all the content
-    return render_template("tutorials_list.html")
+    tutorials = Tutorial.query.all()
+    return render_template("tutorials_list.html", tutorials=tutorials)
+
+
+@main.route("/tutorials/<tutorial_id>")
+def tutorial_details(tutorial_id):
+    """View tutorial content."""
+    # In the future it would be nice if users could track their progress!
+    return render_template("tutorial_detail.html")
